2fbe69bfda1c14ba0180fe3c8c0399c416f3c6a1
Zak <SonZohan@gmail.com>
2015-07-27 15:43:35 -0700

    Fixed Ultrasonic Code to not use disgusting while loops and delays.

---
+++ UltrasonicCylonAda/UltrasonicCylonAda.ino
@@ -0,0 +1,170 @@
+#include "TimerOne.h"
+#include "LPD6803.h"
+
+// How many leds in your strip?
+#define NUM_LEDS 50
+
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+int dataPin = 3;       // 'yellow' wire
+int clockPin = 13;      // 'green' wire
+// Don't forget to connect 'blue' to ground and 'red' to +5V
+
+// Timer 1 is also used by the strip to send pixel clocks
+
+// Set the first variable to the NUMBER of pixels. 20 = 20 pixels in a row
+LPD6803 strip = LPD6803(NUM_LEDS, dataPin, clockPin);
+
+#define echoPin A1 // Echo Pin = Analog Pin 0
+#define trigPin A0 // Trigger Pin = Analog Pin 1
+#define ZERO_COUNT_LIMIT 2
+
+volatile long duration; // Duration used to calculate distance
+volatile long HR_dist=240; // Calculated Distance
+volatile byte zeroCount=0; //Keeps track of the number of '0's seen. Too many 0s = value of 200.
+volatile byte timer2Count;
+
+const int minimumRange=1; //Minimum Sonar range
+const int maximumRange=120; //Maximum Sonar Range. Maximum Range is 200, but we don't want more than 5 feet of behavior.
+
+void setup() { 
+  //Setup the trigger and Echo pins of the HC-SR04 sensor
+  //Serial.begin(9600);
+  //Serial.println("Starting Cylon");
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+  initTimer2();
+  cli();
+  strip.setCPUmax(95);  // start with 50% CPU usage. up this if the strand flickers or is slow
+  // Start up the LED counter
+  strip.begin();
+  //Serial.print("hello");
+  sei();
+}
+
+void loop() { 
+  //Serial.print("hello");
+  // First slide the led in one direction
+  for(int i = 0; i < NUM_LEDS; i++) { 
+    // Set the i'th led to red 
+    int8_t stripValue = maximumRange - HR_dist/2;
+    strip.setPixelColor(i, Color(stripValue, 0, 0));
+    strip.show();
+    // now that we've shown the leds, reset the i'th led to black
+    //strip.setPixelColor(i, Color(0, 0, 0));
+    // Wait a little bit before we loop around and do it again
+    //FastLED.setBrightness( 255-(HR_dist/2)*3 );
+    delay(30+HR_dist);
+    strip.setPixelColor(i, Color(0, 0, 0));
+    
+  }
+
+  // Now go in the other direction.  
+  for(int i = NUM_LEDS-1; i >= 0; i--) {
+    // Set the i'th led to red 
+    int stripValue = maximumRange - (HR_dist/2);
+    strip.setPixelColor(i, Color(stripValue, 0, 0));
+    strip.show();
+    // now that we've shown the leds, reset the i'th led to black
+    //strip.setPixelColor(i, Color(0, 0, 0));
+    // Wait a little bit before we loop around and do it again
+    //FastLED.setBrightness( 255-(HR_dist/2)*3 );
+    delay(30+HR_dist);
+    strip.setPixelColor(i, Color(0, 0, 0));
+  }
+}
+
+// Create a 15 bit color value from R,G,B
+unsigned int Color(byte r, byte g, byte b)
+{
+  //Take the lowest 5 bits of each value and append them end to end
+  return( ((unsigned int)g & 0x1F )<<10 | ((unsigned int)r & 0x1F)<<5 | (unsigned int)b & 0x1F);
+}
+
+
+/*--------------------getDistance() FUNCTION ---------------*/
+void getDistance(){ 
+ 
+ /* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ //delayMicroseconds(2); 
+ 
+ digitalWrite(trigPin, HIGH);
+ //delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ long newDist = duration/58.2;
+ /*Send the reading from the ultrasonic sensor to the computer */
+  if (newDist >= maximumRange || newDist <= minimumRange){
+    if(++zeroCount >= ZERO_COUNT_LIMIT) {
+       zeroCount = 0;
+       HR_dist+=60;
+     }
+     /* Send a 0 to computer and Turn LED ON to indicate "out of range" */
+     //digitalWrite(LEDPin, HIGH); 
+   } else {
+     HR_dist = newDist;
+     /* Send the distance to the computer using Serial protocol, and
+     turn LED OFF to indicate successful reading. */
+     //digitalWrite(LEDPin, LOW);
+  }
+}
+
+// Timer section
+
+void initTimer2() 
+{
+  // I like to disable global interrupts while initializing counter registers.
+  // That may not be necessary, but...
+  cli();
+  int prescale  = 1024;
+  int ocr2aval  = 160;
+
+  // The following are scaled for convenient printing
+  //
+  // Interrupt interval in microseconds
+  float iinterval = prescale * (ocr2aval+1) / (F_CPU / 1.0e6);
+
+  // Period in microseconds
+  float period    = 2.0 * iinterval;
+
+  // Frequency in Hz
+  float freq      = 1.0e6 / period;
+  
+  // Set Timer 2 CTC mode and prescale by 8
+  //
+  // WGM22:0 = 010: CTC Mode
+  // WGM2 bits 1 and 0 are in TCCR2A,
+  // WGM2 bit 2 is in TCCR2B
+  //
+  TCCR2A = (1 << WGM21);
+  
+  // Set Timer 2  No prescaling
+  //
+  // CS22:0 = 010: prescale = 8
+  // CS2 bits 2:0 are all in TCCR2B
+  TCCR2B = (1 << CS20)|(1 << CS21)|(1 << CS22);
+  
+  // Enable Compare-match register A interrupt for timer2
+  TIMSK2 = (1 << OCIE2A);
+  
+  // This value determines the interrupt interval
+  OCR2A = ocr2aval;
+  sei();
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+   timer2Count++;
+   if(timer2Count >= 20) {
+     //Serial.println("Boop");
+     timer2Count = 0;
+     getDistance();
+     if(HR_dist > 240) HR_dist = 240;
+   }
+}
---
+++ UltrasonicRainbow/UltrasonicRainbow.ino
@@ -0,0 +1,170 @@
+#include "TimerOne.h"
+#include "LPD6803.h"
+#include "NewPing.h"
+
+//Example to control LPD6803-based RGB LED Modules in a strand
+// Original code by Bliptronics.com Ben Moyes 2009
+//Use this as you wish, but please give credit, or at least buy some of my LEDs!
+
+// Code cleaned up and Object-ified by ladyada, should be a bit easier to use
+
+/*****************************************************************************/
+
+// Choose which 2 pins you will use for output.
+// Can be any valid output pins.
+int dataPin = 3;       // 'yellow' wire
+int clockPin = 13;      // 'green' wire
+// Don't forget to connect 'blue' to ground and 'red' to +5V
+
+// Timer 1 is also used by the strip to send pixel clocks
+
+// Set the first variable to the NUMBER of pixels. 20 = 20 pixels in a row
+LPD6803 strip = LPD6803(50, dataPin, clockPin);
+
+#define TRIGGER_PIN  A0  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     A1  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 150 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+volatile unsigned int uS;
+
+void setup() {
+  //initTimer2();
+Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
+  // The Arduino needs to clock out the data to the pixels
+  // this happens in interrupt timer 1, we can change how often
+  // to call the interrupt. setting CPUmax to 100 will take nearly all all the
+  // time to do the pixel updates and a nicer/faster display, 
+  // especially with strands of over 100 dots.
+  // (Note that the max is 'pessimistic', its probably 10% or 20% less in reality)
+  
+  strip.setCPUmax(80);  // start with 50% CPU usage. up this if the strand flickers or is slow
+  
+  // Start up the LED counter
+  strip.begin();
+  // Update the strip, to start they are all 'off'
+  strip.show();
+}
+
+
+void loop() {
+  // Some example procedures showing how to display to the pixels
+  
+  //colorWipe(Color(255, 0, 0), 50);
+  //colorWipe(Color(0, 255, 0), 50);
+  //colorWipe(Color(0, 0, 255), 50);
+  //strip.setPixelColor(0, Color(0, 0, 0 ));
+  //strip.setPixelColor(1, Color(63, 0, 0 ));
+
+  //rainbow(50);
+  rainbowCycle(50);
+}
+
+void rainbow(uint8_t wait) {
+  int i, j;
+   
+  for (j=0; j < 96 * 3; j++) {     // 3 cycles of all 96 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel( (i + j) % 96));
+    }  
+    strip.show();   // write all the pixels out
+    uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+    delay(25+uS / US_ROUNDTRIP_CM);
+  }
+}
+
+// Slightly different, this one makes the rainbow wheel equally distributed 
+// along the chain
+void rainbowCycle(uint8_t wait) {
+  int i, j;
+  
+  for (j=0; j < 96 * 5; j++) {     // 5 cycles of all 96 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      int preBrightVal = Wheel( ((i * 96 / strip.numPixels()) + j) % 96);
+      byte distance = ((uS/US_ROUNDTRIP_CM)/4);
+      int brightnessChange = 31-distance;
+      if(distance > 120) brightnessChange = 0;
+      int color = setBrightness(preBrightVal,brightnessChange);
+      
+      strip.setPixelColor(i, color);
+    }  
+    strip.show();   // write all the pixels out
+    uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+    delay(25+uS / US_ROUNDTRIP_CM);
+  }
+}
+
+// fill the dots one after the other with said color
+// good for testing purposes
+void colorWipe(uint16_t c, uint8_t wait) {
+  int i;
+  
+  for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+/* Helper functions */
+
+// Create a 15 bit color value from R,G,B
+unsigned int Color(byte r, byte g, byte b)
+{
+  //Take the lowest 5 bits of each value and append them end to end
+  return( ((unsigned int)g & 0x1F )<<10 | ((unsigned int)b & 0x1F)<<5 | (unsigned int)r & 0x1F);
+}
+
+
+// Val from 0-32
+unsigned int setBrightness(int color, byte brightness)
+{
+  byte r,g,b;
+  r = color & 0x1F;
+  g = (color >>10) & 0x1F;
+  b = (color >>5) & 0x1F;
+  brightness &= 0x1F;
+  //Serial.print(r);
+  //Serial.print(" ");
+  //Serial.print(g);
+  //Serial.print(" ");
+  //Serial.println(b);
+  r = (r*brightness)>>5;
+  g = (g*brightness)>>5;
+  b = (b*brightness)>>5;
+  return(Color(r,g,b));
+}
+
+//Input a value 0 to 127 to get a color value.
+//The colours are a transition r - g -b - back to r
+unsigned int Wheel(byte WheelPos)
+{
+  byte r,g,b;
+  switch(WheelPos >> 5)
+  {
+    case 0:
+      r=31- WheelPos % 32;   //Red down
+      g=WheelPos % 32;      // Green up
+      b=0;                  //blue off
+      break; 
+    case 1:
+      g=31- WheelPos % 32;  //green down
+      b=WheelPos % 32;      //blue up
+      r=0;                  //red off
+      break; 
+    case 2:
+      b=31- WheelPos % 32;  //blue down 
+      r=WheelPos % 32;      //red up
+      g=0;                  //green off
+      break; 
+  }
+  return(Color(r,g,b));
+}
+
+    
+    
